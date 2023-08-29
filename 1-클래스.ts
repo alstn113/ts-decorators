c5a3250ee383ecaf9803b6ba0ba20eff578095aa
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-29 21:22:58 +0900

    1-클래스

--- 1-클래스.ts
+++ 1-클래스.ts
@@ -1,23 +1,37 @@
 // 클래스 데코레이터
-function classDecorator(target: Function) {
-  console.log('Class decorator executed');
-  console.log('Target:', target); // 클래스 생성자 함수
+function ClassDecorator(constructor: typeof PersonClass) {
+  console.log('class decorator initialized');
+
+  const method = constructor.prototype.act;
+  constructor.prototype.act = function (action: string) {
+    method.call(this, `${action} in decorator`); // method를 호출하면서 this를 전달
+    console.log('decorator called in class');
+  };
 }
 
-@classDecorator // decorator는 runtime에 호출된다 (즉, class instance를 생성하지 않아도 호출됨)
-class ExampleClass {
+@ClassDecorator // decorator는 runtime에 호출된다 (즉, class instance를 생성하지 않아도 호출됨)
+// 1. class
+class PersonClass {
+  // 2. property
+  person: string = 'minsoo';
+
   constructor() {
-    console.log('ExampleClass instantiated');
+    console.log('PersonClass instantiated');
+  }
+
+  // 3. accessor
+  get getPseron() {
+    return `My name is ${this.person}`;
+  }
+
+  // 4. method
+  act(action: string) {
+    console.log(`${this.person} is ${action}`);
   }
 }
 
-// 실행 결과
-// Class decorator executed
-// Target: [Function: ExampleClass]
+new PersonClass().act('coding');
 
-// 만약 new ExampleClass()를 통해 클래스 인스턴스를 생성하면 아래와 같이 출력된다.
-
-// 실행 결과
-// Class decorator executed
-// Target: [Function: ExampleClass]
-// ExampleClass instantiated
+// 정리하자면
+// class decorator는 class instance를 생성하지 않아도 호출된다 -> class decorator initailized 출력
+// class decorator는 생성자 함수를 받아서 메소드를 재정의 한다. -> decorator called in class 출력
